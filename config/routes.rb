dd4848c0f7f7f995e08e8c4d286bf01ab21aadfd
gandreottola <gandreottola09@gmail.com>
2019-07-22 11:05:25 -0400

    Setup RESTful route for ideas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :ideas
   # RESTful routes
   resources :examples, except: %i[new edit]
+  resources :ideas, except: %i[new edit]
 
   # Custom routes
   post '/sign-up' => 'users#signup'
